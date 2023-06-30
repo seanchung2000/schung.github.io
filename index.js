9e57970b3f741cfc8d5d281d7dc5a0735f9654cf
Sean Chung <seanchung2000@berkeley.edu>
2023-06-30 14:14:33 -0400

    added hidden navigation bar and transition, flex displays, and anchor links

---
+++ index.js
@@ -0,0 +1,10 @@
+const nav = document.querySelector(".nav");
+let lastScrollY = window.scrollY;
+scroll = window.addEventListener("scroll", () => { 
+  if (lastScrollY < window.scrollY) {
+    nav.classList.add("nav--hidden");
+  } else { 
+    nav.classList.remove("nav--hidden");
+  }
+  lastScrollY = window.scrollY;
+})
