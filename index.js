0c6d45631abd41f6ff215424e004611699fb37ea
Sean Chung <seanchung2000@berkeley.edu>
2023-07-01 19:42:47 -0400

    added fade-in animations for certain texts

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 const nav = document.querySelector(".nav");
 let lastScrollY = window.scrollY;
-scroll = window.addEventListener("scroll", () => { 
+// function scroll() { 
+//   const nav = document.querySelector(".nav");
+//   let lastScrollY = window.scrollY;
+//   window.addEventListener("scroll");
+//   if (lastScrollY < window.scrollY) {
+//     nav.classList.add("nav--hidden");
+//   } else { 
+//     nav.classList.remove("nav--hidden");
+//   }
+//   lastScrollY = window.scrollY;
+//   }
+
+window.addEventListener("scroll", () => { 
   if (lastScrollY < window.scrollY) {
     nav.classList.add("nav--hidden");
   } else { 
@@ -8,3 +20,22 @@ scroll = window.addEventListener("scroll", () => {
   }
   lastScrollY = window.scrollY;
 })
+
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } 
+  }
+}
+
+window.addEventListener("scroll", reveal);
+
+// To check the scroll position on the page load
+reveal();
