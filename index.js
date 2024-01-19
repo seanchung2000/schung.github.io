b8859d42a2a4821f91e34f763e332ff6b66ebcfb
Sean Chung <seanchung2000@berkeley.edu>
2024-01-18 22:56:56 -0500

    scroll

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const nav = document.querySelector(".nav");
 let lastScrollY = window.scrollY;
-// function scroll() { 
-//   const nav = document.querySelector(".nav");
-//   let lastScrollY = window.scrollY;
-//   window.addEventListener("scroll");
-//   if (lastScrollY < window.scrollY) {
-//     nav.classList.add("nav--hidden");
-//   } else { 
-//     nav.classList.remove("nav--hidden");
-//   }
-//   lastScrollY = window.scrollY;
-//   }
+function scroll() { 
+  const nav = document.querySelector(".nav");
+  let lastScrollY = window.scrollY;
+  window.addEventListener("scroll");
+  if (lastScrollY < window.scrollY) {
+    nav.classList.add("nav--hidden");
+  } else { 
+    nav.classList.remove("nav--hidden");
+  }
+  lastScrollY = window.scrollY;
+  }
 
 window.addEventListener("scroll", () => { 
   if (lastScrollY < window.scrollY) {
